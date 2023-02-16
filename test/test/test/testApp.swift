12f74a3408430f3ef80ae6cfce7b4c2ea59c5a5a
Paul0808 <58037469+Paul0808@users.noreply.github.com>
2023-02-16 15:33:37 +0100

    test backend part for app

---
+++ test/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by P. Pintea on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

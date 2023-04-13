849939d1ff6a7307d47e4a7761ed9fadce19f376
QingkaiZhu <q.zhu.2@student.rug.nl>
2023-04-14 00:12:50 +0200

    Update GameOverView

--- test/test/test/Views/GameOverView.swift
+++ test/test/test/Views/GameOverView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Kai on 2023/4/11.
 //
-
 import SwiftUI
 
 struct GameOverView: View {
@@ -12,17 +11,44 @@ struct GameOverView: View {
     @StateObject var game: HGViewModel
 
     var body: some View {
-        VStack {
-            Text("Game Over")
-            Text("Winner: \(game.winner)")
-            Button(action: {
-                game.gameOver = false
-                game.showHGView = false
-                game.model = HGModel()
-                StartView(viewModel: game)
-            }, label: {
-                Text("Restart")
-            })
+        ZStack {
+            Image("launcher_pic")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            VStack(spacing: 40) {
+                Text("Game Over")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                VStack {
+                    Text("Winner:")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("\(game.winner)")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                
+                Button(action: {
+                    game.gameOver = false
+                    game.showHGView = false
+                    game.model = HGModel()
+                    StartView(viewModel: game)
+                }, label: {
+                    Text("Restart")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+                        .background(Color("blue3"))
+                        .cornerRadius(10)
+                })
+                .padding(.horizontal)
+            }
         }
     }
 }
